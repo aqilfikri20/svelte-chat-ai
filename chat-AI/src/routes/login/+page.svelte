3f78f1274baeefeca0c9bb9021073041d1e4d8d8
aqilfikri20 <mhdaqilalfikri@gmail.com>
2025-10-27 22:49:13 +0700

    Update

---
+++ chat-AI/src/routes/login/+page.svelte
@@ -0,0 +1,11 @@
+<div>
+    <form>
+        <input placeholder="Masukkan Username"/>
+        <input placeholder="Masukkan Password"/>
+    </form>
+    <button>Login</button>
+    <div>
+        <p>Belum Punya Akun? <a href="/register">Register</a></p>
+        <button>Masuk langsung dengan Gmail</button>
+    </div>
+</div>
--- chat-AI/src/routes/register/+page.svelte
+++ chat-AI/src/routes/register/+page.svelte
@@ -1,7 +1,16 @@
 <div>
     <form>
-        <input placeholder="Masukkan username"/>
-        <input placeholder="Masukkan Password"/>
+        <input type="text" placeholder="Masukkan username"/>
+        <input type="password" placeholder="Masukkan Password"/>
+        <input type="password" placeholder="Konfirmasi Password"/>
+        <div>
+            <label>Verivikasi dengan:</label><br>
+            <input type="radio" name="gmail" value="gmail" > gmail
+            <input type="radio" name="telp" value="telp"> Telepon
+            <button>Kirim Kode</button>
+        </div>
+        <input placeholder="Masukkan Kode Verivikasi"/>
+
     </form>
-    <button>Login</button>
+    <button type="submit">Login</button>
 </div>
